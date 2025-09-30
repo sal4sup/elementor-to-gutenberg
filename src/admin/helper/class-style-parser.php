382fbac2af0c438272e6610322d2b3608f2c115b
sal4sup <sal4sup@gmail.com>
2025-09-30 13:12:11 +0300

    Add layout helpers (Builder, Style, Classifier, Widths) and switch Admin_Settings to Columns-first with real wrappers

--- src/admin/helper/class-style-parser.php
+++ src/admin/helper/class-style-parser.php
@@ -4,6 +4,7 @@
  *
  * @package Progressus\Gutenberg
  */
+
 namespace Progressus\Gutenberg\Admin\Helper;
 
 defined( 'ABSPATH' ) || exit;
@@ -16,6 +17,7 @@ class Style_Parser {
 	 * Parse typography settings from Elementor settings.
 	 *
 	 * @param array $settings The Elementor settings array.
+	 *
 	 * @return array Array containing 'attributes' and 'style' keys.
 	 */
 	public static function parse_typography( array $settings ): array {
@@ -23,14 +25,14 @@ class Style_Parser {
 		$style = '';
 
 		$typography_fields = array(
-			'typography_font_family'     => array( 'attr' => 'fontFamily', 'style' => 'font-family' ),
-			'typography_font_size'       => array( 'attr' => 'fontSize', 'style' => 'font-size', 'is_array' => true ),
-			'typography_font_weight'     => array( 'attr' => 'fontWeight', 'style' => 'font-weight' ),
-			'typography_line_height'     => array( 'attr' => 'lineHeight', 'style' => 'line-height', 'is_array' => true ),
-			'typography_font_style'      => array( 'attr' => 'fontStyle', 'style' => 'font-style' ),
-			'typography_text_decoration' => array( 'attr' => 'textDecoration', 'style' => 'text-decoration' ),
-			'typography_letter_spacing'  => array( 'attr' => 'letterSpacing', 'style' => 'letter-spacing', 'is_array' => true ),
-			'typography_word_spacing'    => array( 'attr' => 'wordSpacing', 'style' => 'word-spacing', 'is_array' => true ),
+		'typography_font_family'     => array( 'attr' => 'fontFamily', 'style' => 'font-family' ),
+		'typography_font_size'       => array( 'attr' => 'fontSize', 'style' => 'font-size', 'is_array' => true ),
+		'typography_font_weight'     => array( 'attr' => 'fontWeight', 'style' => 'font-weight' ),
+		'typography_line_height'     => array( 'attr' => 'lineHeight', 'style' => 'line-height', 'is_array' => true ),
+		'typography_font_style'      => array( 'attr' => 'fontStyle', 'style' => 'font-style' ),
+		'typography_text_decoration' => array( 'attr' => 'textDecoration', 'style' => 'text-decoration' ),
+		'typography_letter_spacing'  => array( 'attr' => 'letterSpacing', 'style' => 'letter-spacing', 'is_array' => true ),
+		'typography_word_spacing'    => array( 'attr' => 'wordSpacing', 'style' => 'word-spacing', 'is_array' => true ),
 		);
 
 		foreach ( $typography_fields as $key => $field ) {
@@ -42,19 +44,19 @@ class Style_Parser {
 				$size = $settings[ $key ]['size'] ?? '';
 				$unit = $settings[ $key ]['unit'] ?? ( 'typography_line_height' === $key ? '' : 'px' );
 				if ( '' !== $size ) {
-					$value = $size . $unit;
+					$value                  = $size . $unit;
 					$attrs[ $field['attr'] ] = $value;
-					$style .= sprintf( '%s:%s;', $field['style'], esc_attr( $value ) );
+					$style                 .= sprintf( '%s:%s;', $field['style'], esc_attr( $value ) );
 				}
 			} else {
 				$attrs[ $field['attr'] ] = $settings[ $key ];
-				$style .= sprintf( '%s:%s;', $field['style'], esc_attr( $settings[ $key ] ) );
+				$style                 .= sprintf( '%s:%s;', $field['style'], esc_attr( $settings[ $key ] ) );
 			}
 		}
 
 		return array(
-			'attributes' => $attrs,
-			'style'      => $style,
+		'attributes' => $attrs,
+		'style'      => $style,
 		);
 	}
 
@@ -62,21 +64,87 @@ class Style_Parser {
 	 * Parse spacing settings from Elementor settings.
 	 *
 	 * @param array $settings The Elementor settings array.
+	 *
 	 * @return array Array containing style attributes for spacing.
 	 */
 	public static function parse_spacing( array $settings ): array {
-		$attrs = array( 'style' => array( 'spacing' => array() ) );
-		$spacing_types = array( 'margin', 'padding' );
+		$spacing = array();
 
-		foreach ( $spacing_types as $spacing ) {
-			$spacing_key = '_' . $spacing;
-			if ( ! isset( $settings[ $spacing_key ] ) || ! is_array( $settings[ $spacing_key ] ) ) {
+		foreach ( array( '_margin' => 'margin', 'margin' => 'margin', '_padding' => 'padding', 'padding' => 'padding' ) as $key => $type ) {
+			if ( empty( $settings[ $key ] ) || ! is_array( $settings[ $key ] ) ) {
 				continue;
 			}
 
+			$box      = $settings[ $key ];
+			$unit     = isset( $box['unit'] ) ? (string) $box['unit'] : 'px';
+			$unit     = '' === $unit ? 'px' : $unit;
+			$resolved = array();
+
 			foreach ( array( 'top', 'right', 'bottom', 'left' ) as $side ) {
-				if ( isset( $settings[ $spacing_key ][ $side ] ) ) {
-					$attrs['style']['spacing'][ $spacing ][ $side ] = $settings[ $spacing_key ][ $side ] . ( $settings[ $spacing_key ]['unit'] ?? 'px' );
+				if ( ! isset( $box[ $side ] ) ) {
+					continue;
+				}
+
+				$value = self::resolve_dimension( $box[ $side ], $unit );
+				if ( '' !== $value ) {
+					$resolved[ $side ] = $value;
+				}
+			}
+
+			if ( ! empty( $resolved ) ) {
+				$spacing[ $type ] = $resolved;
+			}
+		}
+
+		$gap_keys = array( 'gap', 'gap_columns', 'column_gap' );
+		foreach ( $gap_keys as $gap_key ) {
+			if ( empty( $settings[ $gap_key ] ) ) {
+				continue;
+			}
+
+			$value = self::resolve_dimension( $settings[ $gap_key ], 'px' );
+			if ( '' !== $value ) {
+				$spacing['blockGap'] = $value;
+				break;
+			}
+		}
+
+		$result = array( 'style' => array() );
+		if ( ! empty( $spacing ) ) {
+			$result['style']['spacing'] = $spacing;
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Parse border settings from Elementor settings.
+	 *
+	 * @param array $settings Elementor settings array.
+	 *
+	 * @return array
+	 */
+	public static function parse_border( array $settings ): array {
+		$attrs = array();
+
+		if ( isset( $settings['border_radius'] ) && is_array( $settings['border_radius'] ) ) {
+			$radius_unit = isset( $settings['border_radius']['unit'] ) ? $settings['border_radius']['unit'] : 'px';
+			foreach ( array( 'top', 'right', 'bottom', 'left' ) as $side ) {
+				if ( isset( $settings['border_radius'][ $side ] ) ) {
+					$attrs['style']['border']['radius'][ $side ] = self::resolve_dimension( $settings['border_radius'][ $side ], $radius_unit );
+				}
+			}
+		}
+
+		if ( isset( $settings['border_border'] ) && '' !== $settings['border_border'] ) {
+			$attrs['style']['border']['style'] = $settings['border_border'];
+		}
+
+		if ( isset( $settings['border_width'] ) && is_array( $settings['border_width'] ) ) {
+			$width_unit = isset( $settings['border_width']['unit'] ) ? $settings['border_width']['unit'] : 'px';
+			foreach ( array( 'top', 'right', 'bottom', 'left' ) as $side ) {
+				if ( isset( $settings['border_width'][ $side ] ) ) {
+					$attrs['style']['border']['width'][ $side ] = self::resolve_dimension( $settings['border_width'][ $side ], $width_unit );
 				}
 			}
 		}
@@ -84,30 +152,93 @@ class Style_Parser {
 		return $attrs;
 	}
 
-	public static function parse_border( array $settings ) {
-		if ( isset( $settings['border_radius'] ) && is_array( $settings['border_radius'] ) ) {
-			$unit = isset( $settings['border_radius']['unit'] ) ? $settings['border_radius']['unit'] : 'px';
-			foreach ( array( 'top', 'right', 'bottom', 'left' ) as $side ) {
-				
-				if ( isset( $settings['border_radius'][ $side ] ) ) {
-					$attrs['style']['border']['radius'][ $side ] = $settings['border_radius'][ $side ] . $unit;
-				}
-			}
+	/**
+	 * Parse container styles to Gutenberg block attributes subset.
+	 *
+	 * @param array $settings Elementor settings array.
+	 *
+	 * @return array
+	 */
+	public static function parse_container_styles( array $settings ): array {
+		$attrs = array();
+		$style = array();
+
+		$spacing = self::parse_spacing( $settings );
+		if ( ! empty( $spacing['style']['spacing'] ) ) {
+			$style['spacing'] = $spacing['style']['spacing'];
 		}
 
-		if ( isset( $settings['border_border'] ) ) {
-			$attrs['style']['border']['style'] = $settings['border_border'];
+		$background = self::get_background_color( $settings );
+		if ( '' !== $background ) {
+			$style['color']['background'] = $background;
 		}
 
-		if ( isset( $settings['border_width'] ) && is_array( $settings['border_width'] ) ) {
-			$unit = isset( $settings['border_width']['unit'] ) ? $settings['border_width']['unit'] : 'px';
-			foreach ( array( 'top', 'right', 'bottom', 'left' ) as $side ) {
-				if ( isset( $settings['border_width'][ $side ] ) ) {
-					$attrs['style']['border']['width'][ $side ] = $settings['border_width'][ $side ] . $unit;
-				}
-			}
+		if ( isset( $settings['css_classes'] ) && '' !== trim( (string) $settings['css_classes'] ) ) {
+			$attrs['className'] = trim( (string) $settings['css_classes'] );
+		}
+
+		if ( ! empty( $style ) ) {
+			$attrs['style'] = $style;
 		}
 
 		return $attrs;
+	}
+
+	/**
+	 * Resolve a dimension value to a CSS-ready string.
+	 *
+	 * @param mixed  $value Raw Elementor value.
+	 * @param string $fallback_unit Default unit when not provided.
+	 *
+	 * @return string
+	 */
+	private static function resolve_dimension( $value, string $fallback_unit ): string {
+		if ( is_array( $value ) ) {
+			$size = $value['size'] ?? $value['value'] ?? '';
+			$unit = $value['unit'] ?? $fallback_unit;
+			if ( '' === $unit ) {
+				$unit = $fallback_unit;
+			}
+			if ( '' !== $size ) {
+				return $size . $unit;
+			}
+			return '';
+		}
+
+		if ( null === $value || '' === $value ) {
+			return '';
+		}
+
+		$value = (string) $value;
+		if ( 'default' === strtolower( $value ) ) {
+			return '';
+		}
+		if ( preg_match( '/[a-z%]+$/i', $value ) ) {
+			return $value;
+		}
+
+		if ( '' === $fallback_unit ) {
+			$fallback_unit = 'px';
+		}
+
+		return $value . $fallback_unit;
+	}
+
+	/**
+	 * Extract background color from settings.
+	 *
+	 * @param array $settings Elementor settings.
+	 *
+	 * @return string
+	 */
+	private static function get_background_color( array $settings ): string {
+		$keys = array( '_background_color', 'background_color' );
+		foreach ( $keys as $key ) {
+			if ( isset( $settings[ $key ] ) && '' !== (string) $settings[ $key ] ) {
+				return (string) $settings[ $key ];
+			}
+		}
+
+		return '';
 	}
 }
---
+++ src/admin/layout/class-container-classifier.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * Decide which Gutenberg layout to use when rendering Elementor containers.
+ *
+ * @package Progressus\Gutenberg
+ */
+
+namespace Progressus\Gutenberg\Admin\Layout;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Determine which layout a container should use.
+ */
+class Container_Classifier {
+	/**
+	 * Determine if a container should render as a grid.
+	 *
+	 * @param array $element Elementor container element.
+	 *
+	 * @return bool
+	 */
+	public static function is_grid( array $element ): bool {
+		$settings = $element['settings'] ?? array();
+		$child_count = isset( $element['elements'] ) && is_array( $element['elements'] ) ? count( $element['elements'] ) : 0;
+
+		if ( isset( $settings['layout'] ) && 'grid' === $settings['layout'] ) {
+			return true;
+		}
+
+		if ( isset( $settings['display'] ) && 'grid' === $settings['display'] ) {
+			return true;
+		}
+
+		if ( isset( $settings['grid_columns'] ) || isset( $settings['grid_template_columns'] ) || isset( $settings['grid_auto_flow'] ) ) {
+			return true;
+		}
+
+		if ( isset( $settings['grid_row_gap'] ) || isset( $settings['gap_rows'] ) ) {
+			return true;
+		}
+
+		if ( $child_count > 4 ) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Determine whether we should render a container as columns.
+	 *
+	 * @param array $element  Elementor container element.
+	 * @param array $children Children elements.
+	 *
+	 * @return bool
+	 */
+	public static function should_render_columns( array $element, array $children ): bool {
+		$child_count = count( $children );
+		if ( $child_count < 2 || $child_count > 4 ) {
+			return false;
+		}
+
+		if ( self::is_grid( $element ) ) {
+			return false;
+		}
+
+		$settings = $element['settings'] ?? array();
+		$direction = self::get_flex_direction( $settings );
+		if ( 'column' === $direction || 'column-reverse' === $direction ) {
+			return false;
+		}
+
+		$wrap = $settings['flex_wrap'] ?? $settings['flex_wrap_mobile'] ?? '';
+		if ( in_array( $wrap, array( 'wrap', 'wrap-reverse' ), true ) && $child_count > 3 ) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Infer a grid column count from Elementor settings.
+	 *
+	 * @param array $element     Elementor container element.
+	 * @param int   $child_count Number of children.
+	 *
+	 * @return int
+	 */
+	public static function get_grid_column_count( array $element, int $child_count ): int {
+		$settings = $element['settings'] ?? array();
+		$possible_keys = array( 'grid_columns', 'columns', 'grid_columns_number', 'grid_template_columns' );
+
+		foreach ( $possible_keys as $key ) {
+			if ( empty( $settings[ $key ] ) ) {
+				continue;
+			}
+
+			$value = $settings[ $key ];
+			if ( is_array( $value ) ) {
+				$value = $value['size'] ?? reset( $value );
+			}
+
+			$value = (int) $value;
+			if ( $value > 0 ) {
+				return min( max( 1, $value ), max( 1, $child_count ) );
+			}
+		}
+
+		if ( $child_count >= 4 ) {
+			return 4;
+		}
+
+		if ( $child_count >= 3 ) {
+			return 3;
+		}
+
+		if ( $child_count > 0 ) {
+			return $child_count;
+		}
+
+		return 1;
+	}
+
+	/**
+	 * Get the flex direction configured for a container.
+	 *
+	 * @param array $settings Elementor settings array.
+	 *
+	 * @return string
+	 */
+	public static function get_flex_direction( array $settings ): string {
+		$direction = $settings['flex_direction'] ?? $settings['direction'] ?? '';
+
+		$valid = array( 'row', 'row-reverse', 'column', 'column-reverse' );
+		if ( in_array( $direction, $valid, true ) ) {
+			return $direction;
+		}
+
+		return 'row';
+	}
+}
